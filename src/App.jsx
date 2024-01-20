276fe98349214029cb75ace82dc167e63ae36a21
Margulis162 <markmargulis@proton.me>
2024-01-20 14:40:01 -0500

    Just keep following the basic tutorial on React. Made the Square components to remember states

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-export default function Square() {
+import {useState} from 'react'
+
+function Square(){
+
+  const[value, setValue] = useState('')
+  const[turn, setTurn] = useState(0)
+  
+  function handleClick(){
+    setValue("X")
+  }
+  return (
+  <button
+   className="square"
+   onClick={handleClick}
+   >
+  {value}
+  </button>)
+}
+
+export default function Board() {
   return (
     <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+           <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
