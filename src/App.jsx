3cd684f0cebd90518d40cc19f7c5f48efc3626ef
Margulis162 <markmargulis@proton.me>
2024-01-20 20:39:30 -0500

    a functional version of it

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,32 @@ function Square({value, onSquareClick}){
 }
 
 export default function Board() {
-
+  const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
+
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = "Winner: " + winner
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
 
 
   function handleClick(i) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
+    const nextSquares = squares.slice()
+    if(squares[i] ||calculateWinner(squares)){
+      return
+    }
+    xIsNext?  nextSquares[i] = "X": nextSquares[i] ="O"
+   
+    setSquares(nextSquares)
+    setXIsNext(!xIsNext)
   }
 
   return (
     <>
+          <div className="status">{status}</div>
            <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}  />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -46,5 +60,26 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
-  );
+  ) 
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0  i < lines.length  i++) {
+    const [a, b, c] = lines[i] 
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a] 
+    }
+  }
+  return null 
 }
